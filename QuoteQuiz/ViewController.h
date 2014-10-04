2a4ac528a598845b7a9dc959cdb39e6f1d4f8733
Goodman <jarryd999@oscr-mz151.catnet.arizona.edu>
2014-10-03 18:01:08 -0700

    Initial Commit

---
+++ QuoteQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuoteQuiz
+//
+//  Created by Goodman, Jarryd A - (jarryd999) on 10/3/14.
+//  Copyright (c) 2014 Goodman, Jarryd A - (jarryd999). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
